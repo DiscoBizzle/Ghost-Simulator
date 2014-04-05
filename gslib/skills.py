8279b2873e5572eacd17a1713a46ea96457a9f27
Michael <themathsmassage@gmail.com>
2014-04-05 22:38:04 +0100

    Skill system stuff

--- gslib/skills.py
+++ gslib/skills.py
@@ -3,6 +3,7 @@ __author__ = 'Michael'
 import json
 import os
 from constants import *
+import player
 
 class Skill(object):
     def __init__(self, name, prereqs, effect):
@@ -10,12 +11,19 @@ class Skill(object):
         self.prereqs = prereqs
         self.effect = effect
 
+    def can_be_learnt(self, player):
+        if self.name in player.skills_learnt:
+            return False
+        for skill in self.prereqs:
+            if skill not in player.skills_learnt:
+                return False
+        return True
 
 def load_skill_dict():
     raw_skill_dict = json.load(open(SKILLS_FILE))
     skill_dict = {}
     for key in raw_skill_dict:
-        skill_dict[key] = Skill(raw_skill_dict[key]['name'],raw_skill_dict[key]['prereqs'],raw_skill_dict[key]['effect'])
+        skill_dict[key] = Skill(key,raw_skill_dict[key]['prereqs'],raw_skill_dict[key]['effect'])
     return skill_dict
 
 
