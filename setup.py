0db607568f2ec87f7931382a1f78baa39ee85819
Josh Holland <jrh@joshh.co.uk>
2014-04-05 20:54:45 +0100

    Add initial setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(name="ghost-simulator",
+      version="0.1",
+      packages=['gslib'],
+      scripts=['ghost-simulator.py'],
+      data_files=[('gs-chars', ['characters/' + fname for fname in [
+                    'bio.txt', 'fears.txt', 'first_names_f.txt',
+                    'first_names_m.txt', 'second_names.txt',
+                    'object_description.txt',
+                    'ghostSheet.png'
+            ]]),
+                  ('gs-tiles', ['tiles/' + fname for fname in [
+                    'martin.json', 'martin.png']])],
+      )
