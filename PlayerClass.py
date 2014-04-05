c479a4b1eefe7a273757c6c063dd8e9e4047ef0d
MartinHowarth <howarth.martin@gmail.com>
2014-04-05 13:32:24 +0100

    Merge branch 'master' of https://github.com/DiscoBizzle/Ghost-Simulator
    Conflicts:
    	PlayerClass.py

--- PlayerClass.py
+++ PlayerClass.py
@@ -4,16 +4,16 @@ import pygame
 
 def check_screen_collision(p):
     pro_pos = (p.coord[0] + p.velocity[0], p.coord[1] + p.velocity[1])
-    if pro_pos[0] > 0 and pro_pos[0] + p.dimensions[0] < WINDOW_WIDTH and \
-       pro_pos[1] > 0 and pro_pos[1] + p.dimensions[1] < WINDOW_HEIGHT:
+    if pro_pos[0] >= 0 and pro_pos[0] + p.dimensions[0] <= WINDOW_WIDTH and \
+       pro_pos[1] >= 0 and pro_pos[1] + p.dimensions[1] <= WINDOW_HEIGHT:
         return pro_pos
-    elif p.velocity[0] < 0 and pro_pos[0] > 0:
+    elif p.velocity[0] <= 0 and pro_pos[0] <= 0:
         return (0, pro_pos[1])
-    elif p.velocity[1] < 0 and pro_pos[1] > 0:
+    elif p.velocity[1] <= 0 and pro_pos[1] <= 0:
         return (pro_pos[0], 0)
-    elif p.velocity[0] > 0 and pro_pos[0] < WINDOW_WIDTH:
+    elif p.velocity[0] >= 0 and pro_pos[0] >= WINDOW_WIDTH:
         return (WINDOW_WIDTH, pro_pos[1])
-    elif p.velocity[1] > 0 and pro_pos[1] < WINDOW_HEIGHT:
+    elif p.velocity[1] >= 0 and pro_pos[1] >= WINDOW_HEIGHT:
         return (pro_pos[0], WINDOW_HEIGHT)
 
     return p.coord
