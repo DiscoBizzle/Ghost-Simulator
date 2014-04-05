6c6e006775b1dc7cf4a727bbeb81b7bf7ae2b7ae
MartinHowarth <howarth.martin@gmail.com>
2014-04-05 13:07:56 +0100

    constrained to window

--- PlayerClass.py
+++ PlayerClass.py
@@ -1,8 +1,20 @@
+from Constants import *
+
+
+def check_screen_collision(p):
+    if p.coord[0] > 0 and p.coord[0] + p.dimensions[0] < WINDOW_WIDTH and \
+       p.coord[1] > 0 and p.coord[1] + p.dimensions[1] < WINDOW_HEIGHT:
+        return True
+    else:
+        return False
+
+
 class Player:
     def __init__(self,x,y,w,h):
-        self.coord = (x,y)
+        self.coord = (x,y)  # top left
         self.dimensions = (w,h)
         self.velocity = (0,0)
 
     def update(self):
-        self.coord = (self.coord[0] + self.velocity[0], self.coord[1] + self.velocity[1])
+        if check_screen_collision(self):
+            self.coord = (self.coord[0] + self.velocity[0], self.coord[1] + self.velocity[1])
