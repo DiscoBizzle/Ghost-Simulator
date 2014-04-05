fbda1bb6b63c9e4906ae36bc9e08c769876dd51d
MartinHowarth <howarth.martin@gmail.com>
2014-04-05 15:17:52 +0100

    Merge branch 'master' of https://github.com/DiscoBizzle/Ghost-Simulator
    Conflicts:
    	PlayerClass.py

--- PlayerClass.py
+++ PlayerClass.py
@@ -4,19 +4,26 @@ import pygame
 
 def check_screen_collision(p):
     pro_pos = (p.coord[0] + p.velocity[0], p.coord[1] + p.velocity[1])
-    if pro_pos[0] >= 0 and pro_pos[0] + p.dimensions[0] <= GAME_WIDTH and \
-       pro_pos[1] >= 0 and pro_pos[1] + p.dimensions[1] <= GAME_HEIGHT:
+    if pro_pos[0] >= 0 and pro_pos[0] + p.dimensions[0] <= LEVEL_WIDTH and \
+       pro_pos[1] >= 0 and pro_pos[1] + p.dimensions[1] <= LEVEL_HEIGHT:
         return pro_pos
-    elif p.velocity[0] <= 0 and pro_pos[0] <= 0:
-        return (0, pro_pos[1])
-    elif p.velocity[1] <= 0 and pro_pos[1] <= 0:
-        return (pro_pos[0], 0)
-    elif p.velocity[0] >= 0 and pro_pos[0] >= GAME_WIDTH:
-        return (GAME_WIDTH, pro_pos[1])
-    elif p.velocity[1] >= 0 and pro_pos[1] >= GAME_HEIGHT:
-        return (pro_pos[0], GAME_HEIGHT)
 
-    return p.coord
+    x, y = pro_pos
+
+    if p.velocity[0] < 0:
+        if p.coord[0] + p.velocity[0] <= 0:
+            x = 0
+    elif p.velocity[0] > 0:
+        if p.coord[0] + p.dimensions[0] + p.velocity[0] >= LEVEL_WIDTH:
+            x = LEVEL_WIDTH - p.dimensions[0]
+    if p.velocity[1] < 0:
+        if p.coord[1] + p.velocity[1] <= 0:
+            y = 0
+    elif p.velocity[1] > 0:
+        if p.coord[1] + p.dimensions[1] + p.velocity[1] >= LEVEL_HEIGHT:
+            y = LEVEL_HEIGHT - p.dimensions[1]
+
+    return x, y
 
 
 class Player:
