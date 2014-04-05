7ba8383370e7808fec28f12d70f387c66dd83204
Dwarr <zangzor@gmail.com>
2014-04-05 16:42:11 +0100

    Working towards making Maps.py load maps.

--- Maps.py
+++ Maps.py
@@ -8,7 +8,7 @@ def test():
     pygame.init()
     pygame.font.init()
     screen = pygame.display.set_mode((800, 800))
-    m = Map('tileset_test.png', [[Tile(i*j) for i in range(10)] for j in range(10)])
+    m = Map('tileset_test.png', 'tiles/testa.json', [[Tile(i*j) for i in range(10)] for j in range(10)])
 
     surf = Graphics.draw_map(m)
 
@@ -42,15 +42,15 @@ def load_map(map_filename): # Load a map from a map file
     return grid
 
 class Tile(object):
-    def __init__(self, n):
-        self.tileset_coord = (n % 3, 0)
+    def __init__(self, coord):
+        self.tileset_coord = (coord, 0)
         self.tileset_tile_size = TILE_SIZE
         self.tileset_area = (self.tileset_coord[0] * self.tileset_tile_size, self.tileset_coord[1] * self.tileset_tile_size, self.tileset_tile_size, self.tileset_tile_size)
         self.walkable = True
 
 
 class Map(object):
-    def __init__(self, tileset, grid):
+    def __init__(self, tileset, map_file, grid):
         self.tileset = pygame.image.load(tileset).convert()
         self.grid = grid
 
