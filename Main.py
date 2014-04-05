69bbb759ccf9f242850b3b628a7117b94966a2e3
Josh Holland <jrh@joshh.co.uk>
2014-04-05 20:11:42 +0100

    Reorganise Main.py a little

--- Main.py
+++ Main.py
@@ -1,15 +1,16 @@
 import pygame
-pygame.init()
 import GameClass
 import PlayerClass
 from Constants import *
 
 
 def main():
-    Game = GameClass.Game(GAME_WIDTH, GAME_HEIGHT)
+	pygame.init()
 
+	Game = GameClass.Game(GAME_WIDTH, GAME_HEIGHT)
     Game.gameLoop()
 
     pygame.quit()
 
-if __name__ == '__main__': main()
+if __name__ == '__main__':
+	main()
