c8fd95a6a01855612ac36749a8b27a47792d01ba
Michael <themathsmassage@gmail.com>
2014-04-05 20:24:02 +0100

    even more slightly improved sound engine.

--- sound.py
+++ sound.py
@@ -3,24 +3,21 @@ __author__ = 'Michael'
 import os
 import pygame
 import random
+from Constants import *
 
 def load_all_sounds():
     sound_dict = {}
-    for f in os.listdir("sounds"):
-        if '.ogg' == f[-4:]:
-            sound_dict[f[:-4]] = pygame.mixer.Sound(os.path.join("Sounds", f))
-        elif '.wav' == f[-4:]:
-            sound_dict[f[:-4]] = pygame.mixer.Sound(os.path.join("Sounds", f))
+    for f in os.listdir(SOUND_DIR):
+        if f[-4:] in (".ogg", ".wav"):
+            sound_dict[f[:-4]] = pygame.mixer.Sound(os.path.join(SOUND_DIR, f))
 
     return sound_dict
 
 def get_music_list():
     musicList = []
-    for f in os.listdir("music"):
-        if '.ogg' == f[-4:]:
-            musicList.append(os.path.join("Music", f))
-        elif '.wav' == f[-4:]:
-            musicList.append(os.path.join("Music", f))
+    for f in os.listdir(MUSIC_DIR):
+        if f[-4:] in (".ogg", ".wav"):
+            musicList.append(os.path.join(MUSIC_DIR, f))
     return musicList
 
 def start_next_music(musicList):
