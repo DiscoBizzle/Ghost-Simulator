f8082a64e48025fd9de522fc87b95180a1a61fb0
Oliver Middleton <olliemail27@gmail.com>
2014-04-05 17:34:01 +0100

    add move to GameObject

--- GameObject.py
+++ GameObject.py
@@ -1,5 +1,34 @@
+from Constants import *
+
+
 class GameObject:
-    def __init__(self,x,y,w,h):
+    def __init__(self, game_class, x, y, w, h):
+        self.game_class = game_class
+
         self.coord = (x,y)  # top left
         self.dimensions = (w,h)
         self.velocity = (0,0)
+        self.game_class = game_class
+
+    def move(self):
+        pro_pos = (self.coord[0] + self.velocity[0], self.coord[1] + self.velocity[1])
+        if pro_pos[0] >= 0 and pro_pos[0] + self.dimensions[0] <= LEVEL_WIDTH and \
+                pro_pos[1] >= 0 and pro_pos[1] + self.dimensions[1] <= LEVEL_HEIGHT:
+            self.coord = pro_pos
+    
+        x, y = pro_pos
+    
+        if self.velocity[0] < 0:
+            if self.coord[0] + self.velocity[0] <= 0:
+                x = 0
+        elif self.velocity[0] > 0:
+            if self.coord[0] + self.dimensions[0] + self.velocity[0] >= LEVEL_WIDTH:
+                x = LEVEL_WIDTH - self.dimensions[0]
+        if self.velocity[1] < 0:
+            if self.coord[1] + self.velocity[1] <= 0:
+                y = 0
+        elif self.velocity[1] > 0:
+            if self.coord[1] + self.dimensions[1] + self.velocity[1] >= LEVEL_HEIGHT:
+                y = LEVEL_HEIGHT - self.dimensions[1]
+
+        self.coord = x, y
--- PlayerClass.py
+++ PlayerClass.py
@@ -4,33 +4,9 @@ import pygame
 from GameObject import GameObject
 
 
-def check_screen_collision(p):
-    pro_pos = (p.coord[0] + p.velocity[0], p.coord[1] + p.velocity[1])
-    if pro_pos[0] >= 0 and pro_pos[0] + p.dimensions[0] <= LEVEL_WIDTH and \
-       pro_pos[1] >= 0 and pro_pos[1] + p.dimensions[1] <= LEVEL_HEIGHT:
-        return pro_pos
-
-    x, y = pro_pos
-
-    if p.velocity[0] < 0:
-        if p.coord[0] + p.velocity[0] <= 0:
-            x = 0
-    elif p.velocity[0] > 0:
-        if p.coord[0] + p.dimensions[0] + p.velocity[0] >= LEVEL_WIDTH:
-            x = LEVEL_WIDTH - p.dimensions[0]
-    if p.velocity[1] < 0:
-        if p.coord[1] + p.velocity[1] <= 0:
-            y = 0
-    elif p.velocity[1] > 0:
-        if p.coord[1] + p.dimensions[1] + p.velocity[1] >= LEVEL_HEIGHT:
-            y = LEVEL_HEIGHT - p.dimensions[1]
-
-    return x, y
-
-
 class Player(GameObject):
-    def __init__(self,x,y,w,h):
-        GameObject.__init__(self, x, y, w, h)
+    def __init__(self, game_object, x, y, w, h):
+        GameObject.__init__(self, game_object, x, y, w, h)
 
         self.animationState = ANIM_IDLE
         self.frameCount = 0 #no. of frames since game started
@@ -42,20 +18,20 @@ class Player(GameObject):
         self.spriteSheet.set_colorkey((255,0,255))
 
 
-    def update(self, gameClass):
+    def update(self):
         # update velocity
         v_x, v_y = 0, 0
         self.animationState = ANIM_IDLE
-        if gameClass.keys[pygame.K_DOWN]:
+        if self.game_class.keys[pygame.K_DOWN]:
             v_y += 5
             self.animationState = ANIM_DOWNWALK
-        if gameClass.keys[pygame.K_UP]:
+        if self.game_class.keys[pygame.K_UP]:
             v_y -= 5
             self.animationState = ANIM_UPWALK
-        if gameClass.keys[pygame.K_LEFT]:
+        if self.game_class.keys[pygame.K_LEFT]:
             v_x -= 5
             self.animationState = ANIM_LEFTWALK
-        if gameClass.keys[pygame.K_RIGHT]:
+        if self.game_class.keys[pygame.K_RIGHT]:
             v_x += 5
             self.animationState = ANIM_RIGHTWALK
 
@@ -71,4 +47,4 @@ class Player(GameObject):
         self.velocity = (v_x, v_y)
 
         # actually move
-        self.coord = check_screen_collision(self)
+        self.move()
