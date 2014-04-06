b8f5d80c6609b2ba62cc4f0b0afb1fe1ff3d3453
Oliver Middleton <olliemail27@gmail.com>
2014-04-06 21:03:13 +0100

    joy: update

--- gslib/joy.py
+++ gslib/joy.py
@@ -3,6 +3,7 @@ import pygame
 from gslib import character
 from gslib.constants import *
 
+
 class JoyController(object):
     def __init__(self, game):
         self.game = game
@@ -12,34 +13,34 @@ class JoyController(object):
 
         if pygame.joystick.get_count() == 0:
             pygame.joystick.quit()
+            return
         else:
             self.joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]
             for joystick in self.joysticks:
                 joystick.init()
 
-
     def handle_hat(self, event):
         x, y = event.value
         if x == -1:
-            self.game.keys[pygame.K_LEFT] = True
+            self.game.players[0].move_left = True
         elif x == 1:
-            self.game.keys[pygame.K_RIGHT] = True
+            self.game.players[0].move_right = True
         elif x == 0:
-            self.game.keys[pygame.K_LEFT] = False
-            self.game.keys[pygame.K_RIGHT] = False
+            self.game.players[0].move_left = False
+            self.game.players[0].move_right = False
         if y == -1:
-            self.game.keys[pygame.K_DOWN] = True
+            self.game.players[0].move_down = True
         elif y == 1:
-            self.game.keys[pygame.K_UP] = True
+            self.game.players[0].move_up = True
         elif y == 0:
-            self.game.keys[pygame.K_DOWN] = False
-            self.game.keys[pygame.K_UP] = False
-
+            self.game.players[0].move_down = False
+            self.game.players[0].move_up = False
 
     def handle_buttondown(self, event):
         if event.button == 0:
-            self.game.objects.append(character.Character(self.game, self.game.player1.coord[0], self.game.player1.coord[1], 16, 16,
-                                                         character.gen_character()))
+            self.game.objects.append(
+                character.Character(self.game, self.game.player1.coord[0], self.game.player1.coord[1], 16, 16,
+                                    character.gen_character()))
         elif event.button == 1:
             if self.game.GameState == MAIN_MENU:
                 self.game.GameState = MAIN_GAME
@@ -52,44 +53,26 @@ class JoyController(object):
         elif event.button == 4:
             self.game.options['torch'] = not self.game.options['torch']
 
-
     def handle_buttonup(self, event):
         pass
-
 
     def handle_axis(self, event):
         if event.axis == 0:
             if event.value < -0.1:
-                self.game.keys[pygame.K_LEFT] = True
+                self.game.players[0].move_left = True
             elif event.value > 0.1:
-                self.game.keys[pygame.K_RIGHT] = True
+                self.game.players[0].move_right = True
             else:
-                self.game.keys[pygame.K_LEFT] = False
-                self.game.keys[pygame.K_RIGHT] = False
+                self.game.players[0].move_left = False
+                self.game.players[0].move_right = False
         elif event.axis == 1:
             if event.value < -0.1:
-                self.game.keys[pygame.K_UP] = True
+                self.game.players[0].move_up = True
             elif event.value > 0.1:
-                self.game.keys[pygame.K_DOWN] = True
+                self.game.players[0].move_down = True
             else:
-                self.game.keys[pygame.K_DOWN] = False
-                self.game.keys[pygame.K_UP] = False
-
+                self.game.players[0].move_down = False
+                self.game.players[0].move_up = False
 
     def handle_ball(self, event):
-        if event.axis == 0:
-            if event.value < -0.1:
-                self.game.keys[pygame.K_LEFT] = True
-            elif event.value > 0.1:
-                self.game.keys[pygame.K_RIGHT] = True
-            else:
-                self.game.keys[pygame.K_LEFT] = False
-                self.game.keys[pygame.K_RIGHT] = False
-        elif event.axis == 1:
-            if event.value < -0.1:
-                self.game.keys[pygame.K_UP] = True
-            elif event.value > 0.1:
-                self.game.keys[pygame.K_DOWN] = True
-            else:
-                self.game.keys[pygame.K_DOWN] = False
-                self.game.keys[pygame.K_UP] = False
+        self.handle_axis(event)
