51301afc8efa92424a3a60a464d7e06218bd7ea2
DiscoBizzle <jonny__105@hotmail.com>
2014-04-06 14:26:26 +0100

    textbox stuff

--- gslib/TextBox.py
+++ gslib/TextBox.py
@@ -9,6 +9,10 @@ CLOSING = 4
 
 class TextBox:
     def __init__(self, text):
+        self.x_pad = 16
+        self.y_pad = 16
+        
+
         self.state = INACTIVE
         self.base_rect = pygame.Rect()
 
