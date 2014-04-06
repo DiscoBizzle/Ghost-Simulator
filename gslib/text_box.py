0b39701dc73f6cd7239b54fe06f9a22de40dba52
DiscoBizzle <jonny__105@hotmail.com>
2014-04-06 15:23:07 +0100

    moved text functions to a sensible place

--- gslib/text_box.py
+++ gslib/text_box.py
@@ -10,15 +10,20 @@ CLOSING = 4
 
 class TextBox:
     def __init__(self, text):
+        self.bg_color = pygame.Color((125, 25, 32))
         self.x_pad = 16
         self.y_pad = 16
         self.h = 256
+        self.portrait = pygame.image.load("characters/portrait_test.png")
 
         self.state = INACTIVE
         self.coord = (self.x_pad, GAME_HEIGHT - self.h - self.y_pad)
         self.base_rect = pygame.Rect(0, 0, GAME_WIDTH - 2*self.x_pad, self.h)
+        self.portrait_rect = pygame.Rect(16, 16, self.h-32, self.h-32)
 
     def create_background_surface(self): # creates a surface for the textbox (minus the text so we can make that scroll)
-        #self.background_surface =
+        self.background_surface = pygame.Surface(self.base_rect.w, self.base_rect.h)
+        self.background_surface.fill(self.bg_color)
+
         pass
 
---
+++ gslib/text_functions.py
@@ -0,0 +1,41 @@
+
+
+def truncline(text, font, maxwidth):
+    real = len(text)
+    stext = text
+    l = font.size(text)[0]
+    cut = 0
+    a = 0
+    done = 1
+    while l > maxwidth:
+        a += 1
+        n = text.rsplit(None, a)[0]
+        if stext == n:
+            cut += 1
+            stext = n[:-cut]
+        else:
+            stext = n
+        l = font.size(stext)[0]
+        real = len(stext)
+        done = 0
+    return real, done, stext
+
+
+def wrapline(text, font, maxwidth):
+    done = 0
+    wrapped = []
+
+    while not done:
+        nl, done, stext = truncline(text, font, maxwidth)
+        wrapped.append(stext.strip())
+        text = text[nl:]
+    return wrapped
+
+
+def text_wrap(text, font, maxwidth):
+    t = text.split('\n')
+    wrapped = []
+    for l in t:
+        wrapped += wrapline(l, font, maxwidth)
+
+    return wrapped
