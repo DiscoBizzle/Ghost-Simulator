3e66fcb4c1dd3b42c33dd8b2b030022e221aa1c0
DiscoBizzle <jonny__105@hotmail.com>
2014-04-06 19:15:12 +0100

    Textbox tweaks

--- gslib/text_box.py
+++ gslib/text_box.py
@@ -18,23 +18,31 @@ class TextBox:
         self.y_ext_pad = 16
         self.x_int_pad = 16
         self.y_int_pad = 16
+        self.x_text_pad = 16
+        self.y_text_pad = 16
 
-        self.h = 256
+
+        self.h = 224
         self.w = GAME_WIDTH - 2*self.x_int_pad
 
-        self.portrait_surface = pygame.image.load("characters/portrait_test.jpg").convert()
+        self.portrait_surface = pygame.image.load("characters/portrait_test2.jpg").convert()
         self.background_surface = pygame.Surface((int(self.w), int(self.h)))  # create surface
 
         self.state = INACTIVE
+
         self.coord = (self.x_ext_pad, GAME_HEIGHT - self.h - self.y_ext_pad)
         self.base_rect = pygame.Rect(0, 0, self.w, self.h)
-        self.portrait_rect = pygame.Rect(self.x_int_pad, self.y_int_pad, self.h-32, self.h-32)
+        self.portrait_rect = pygame.Rect(self.x_int_pad, (self.h/2 - self.portrait_surface.get_height()/2),
+                                         self.portrait_surface.get_width(), self.portrait_surface.get_height())
 
         self.text_frame_rect = pygame.Rect(
             2*self.x_int_pad + self.portrait_rect.w, self.y_int_pad,
-            self.w - 2*self.x_int_pad, self.h - 2*self.y_int_pad)
+            self.w - 3*self.x_int_pad - self.portrait_rect.w, self.h - 2*self.y_int_pad)
 
     def create_background_surface(self):  # creates a surface for the textbox (minus the text so we can make that scroll)
         self.background_surface.fill(self.bg_color)  # fill background
         self.background_surface.blit(self.portrait_surface, self.portrait_rect)  # blit portrait
         pygame.draw.rect(self.background_surface, self.text_frame_color, self.text_frame_rect)  # fill text background
+
+    def create_text_surfaces(self):
+        pass
