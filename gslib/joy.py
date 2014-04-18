1f147676e8dc3df0d4dde28204cb944851ea7a09
Oliver Middleton <olliemail27@gmail.com>
2014-04-18 06:23:20 +0100

    mess with joystick handling some more

--- gslib/joy.py
+++ gslib/joy.py
@@ -11,61 +11,51 @@ class JoyController(object):
         self.joysticks = input.get_joysticks()
         for joystick in self.joysticks:
             joystick.open()
-            joystick.set_handler('on_joyaxis_motion', self.on_joyaxis_motion)
-            joystick.set_handler('on_joybutton_press', self.on_joybutton_press)
-            joystick.set_handler('on_joybutton_release', self.on_joybutton_release)
-            joystick.set_handler('on_joyhat_motion', self.on_joyhat_motion)
+            joystick.push_handlers(self)
 
     def on_joyaxis_motion(self, joystick, axis, value):
+        player1 = self.game.players['player1']
+        player2 = self.game.players['player2']
         if axis == 'x':
-            if value < -0.1:
-                self.game.players['player1'].move_left = True
-            elif value > 0.1:
-                self.game.players['player1'].move_right = True
-            else:
-                self.game.players['player1'].move_left = False
-                self.game.players['player1'].move_right = False
+            player1.move_left = value < -0.1
+            player1.move_right = value > 0.1
         elif axis == 'y':
-            if value < -0.1:
-                self.game.players['player1'].move_up = True
-            elif value > 0.1:
-                self.game.players['player1'].move_down = True
-            else:
-                self.game.players['player1'].move_down = False
-                self.game.players['player1'].move_up = False
+            player1.move_up = value < -0.1
+            player1.move_down = value > 0.1
+        if axis == 'rz':
+            player2.move_left = value < -0.1
+            player2.move_right = value > 0.1
+        elif axis == 'z':
+            player2.move_up = value < -0.1
+            player2.move_down = value > 0.1
 
     def on_joybutton_press(self, joystick, button):
         if button == 0:
-            self.game.objects['testguy'] = character.Character(self.game, self.game.players['player1'].coord[0],
-                                                               self.game.players['player1'].coord[1], 16, 16,
-                                                               character.gen_character())
+            self.game.players['player1'].harvest_fear()
         elif button == 1:
+            self.game.players['player1'].toggle_possess()
+        elif button == 2:
+            self.game.show_fears = not self.game.show_fears
+        elif button == 3:
+            self.game.show_ranges = not self.game.show_ranges
+        elif button == 4:
+            self.game.options['FOV'] = not self.game.options['FOV']
+        elif button == 5:
+            self.game.options['VOF'] = not self.game.options['VOF']
+        elif button == 6:
+            self.game.options['torch'] = not self.game.options['torch']
+        elif button == 9:
             if self.game.GameState == MAIN_MENU:
                 self.game.set_state(MAIN_GAME)
             elif self.game.GameState == MAIN_GAME or self.game.GameState == GAME_OVER:
                 self.game.set_state(MAIN_MENU)
-        elif button == 2:
-            self.game.options['FOV'] = not self.game.options['FOV']
-        elif button == 3:
-            self.game.options['VOF'] = not self.game.options['VOF']
-        elif button == 4:
-            self.game.options['torch'] = not self.game.options['torch']
 
     def on_joybutton_release(self, joystick, button):
         pass
 
     def on_joyhat_motion(self, joystick, hat_x, hat_y):
-        if hat_x == -1:
-            self.game.players['player1'].move_left = True
-        elif hat_x == 1:
-            self.game.players['player1'].move_right = True
-        elif hat_x == 0:
-            self.game.players['player1'].move_left = False
-            self.game.players['player1'].move_right = False
-        if hat_y == -1:
-            self.game.players['player1'].move_down = True
-        elif hat_y == 1:
-            self.game.players['player1'].move_up = True
-        elif hat_y == 0:
-            self.game.players['player1'].move_down = False
-            self.game.players['player1'].move_up = False
+        player = self.game.players['player1']
+        player.move_left = hat_x == -1
+        player.move_right = hat_x == 1
+        player.move_down = hat_y == -1
+        player.move_up = hat_y == 1
