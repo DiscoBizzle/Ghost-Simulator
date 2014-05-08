f220de653a5328419ea1a98bbee2e802f4b4d0a6
Josh Holland <jrh@joshh.co.uk>
2014-05-08 23:13:16 +0100

    Cleaned up list_box.py

--- gslib/list_box.py
+++ gslib/list_box.py
@@ -1,16 +1,5 @@
 from gslib import button
-
-
-def create_property(var):  # creates a member variable that redraws the button when changed.
-    def _setter(self, val):
-        setattr(self, '_' + var, val)
-        self.update_buttons()
-        # self.redraw()
-
-    def _getter(self):
-        return getattr(self, '_' + var)
-
-    return property(_getter, _setter)
+from gslib.utils import ExecOnChange, exec_on_change_meta
 
 
 def list_func(owner, val):
@@ -27,23 +16,32 @@ def list_func(owner, val):
 
 
 class List(object):
-    def __init__(self, owner, items, function=None, pos=(50, 50), size=(100, 20), visible=True, enabled=True, color=(120, 0, 0),
-                 border_color=(120, 50, 80), border_width=2, text=None, font_size=10, labels='dictkey', **kwargs):
-        self._pos = pos
 
-        self._size = size
-        self._color = color
-        self._visible = visible
-        self._border_color = border_color
-        self._border_width = border_width
-        self._text = text
-        self._font_size = font_size
+    __metaclass__ = exec_on_change_meta(["update_buttons"])
+
+    visible = ExecOnChange
+    size = ExecOnChange
+    color = ExecOnChange
+    border_color = ExecOnChange
+    border_width = ExecOnChange
+    text = ExecOnChange
+    font_size = ExecOnChange
+    selected_name = ExecOnChange
+    pos = ExecOnChange
+
+    def __init__(self, owner, items, function=None, pos=(50, 50), size=(100, 20), visible=True, enabled=True, color=(120, 0, 0),
+                 border_color=(120, 50, 80), border_width=2, text=None, font_size=10, labels='dictkey'):
+        self.pos = pos
+        self.size = size
+        self.color = color
+        self.visible = visible
+        self.border_color = border_color
+        self.border_width = border_width
+        self.text = text
+        self.font_size = font_size
         self.enabled = enabled  # whether button can be activated, visible or not
         self.labels = labels
         #self.priority = False
-
-        for arg in kwargs:  # allows for additional arbitrary arguments to be passed in, useful for more complicated functions
-            setattr(self, arg, kwargs[arg])
 
         self.owner = owner  # container that created the button, allows for the button function to interact with its creator
 
@@ -53,7 +51,7 @@ class List(object):
         self.drop_buttons = []
         self.refresh()
 
-        self._open = True
+        self.open = True
         self.function = function
 
         self.high_color = (0, 120, 0)
@@ -62,25 +60,16 @@ class List(object):
         self.update_buttons()
         # self.redraw()
 
-    # all below variables affect the button surface, so make them properties to redraw on change
-    visible = create_property('visible')
-    size = create_property('size')
-    color = create_property('color')
-    border_color = create_property('border_color')
-    border_width = create_property('border_width')
-    text = create_property('text')
-    font_size = create_property('font_size')
-    selected_name = create_property('selected_name')
-    pos = create_property('pos')
-
-    def get_open(self):
+    @property
+    def open(self):
         return self._open
-    def set_open(self, n):
+
+    @open.setter
+    def open(self, n):
         self._open = n
         #self.priority = n
         #for b in self.drop_buttons:
         #    b.priority = n
-    open = property(get_open, set_open)
 
     def refresh(self):  # call if the list changes
         self.drop_buttons = []
