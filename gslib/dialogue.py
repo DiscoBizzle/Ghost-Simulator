ff902c48e6bca75335a3e8a0118d3a3dd188f930
leafi <leaf@leaf.sx>
2014-05-11 18:09:38 +0100

    dialogue loader & parser

--- gslib/dialogue.py
+++ gslib/dialogue.py
@@ -1,7 +1,76 @@
+import collections
+
 from gslib.constants import *
 from gslib import graphics
 from gslib import rect
 from gslib import text
+
+
+def load_dialogue(filename):
+    d = collections.OrderedDict({"# None": []})
+
+    # First pass - organize lines by heading
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+
+        # filter blank lines
+        lines = filter(lambda l: l.lstrip() != "", lines)
+
+        # strip lines that start with '('
+        lines = filter(lambda l: l.lstrip()[0] != '(', lines)
+
+        # key lines by preceding heading.
+        last_heading = "# None"
+
+        for l in lines:
+            l = l.replace('\r', '').replace('\n', '')
+
+            if l.lstrip()[0] == '#':
+                if l in d:
+                    raise Exception("Duplicate heading '" + l + "' in " + filename)
+                d[l] = []
+                last_heading = l
+            else:
+                # Quick syntax check:
+                # Remaining lines *must* be 'SomeName: blahablah' or 'GOTO whatever' or 'PLAY c_00' or '* some choice'.
+                fword = l.lstrip().split(' ')[0]
+                if not (fword[-1:] == ':' or fword == 'GOTO' or fword == 'PLAY' or fword == '*'):
+                    raise Exception("Syntax error in dialogue file " + filename + ": expected '# some heading' " +
+                                    "or '* some choice' or 'somename: blahblah' or 'PLAY d_cutscene_00' or " +
+                                    "'GOTO ## some heading', but got '" + l + "'.")
+
+                # Line's okay? Good.
+                d[last_heading].append(l)
+
+    # Second pass - find & collate dialogue branches.
+    d2 = collections.OrderedDict()
+    for heading, lines in d.iteritems():
+        new_lines = []
+
+        choice_name = None  # Non-choice lines are represented with the None choice.
+        choice_lines_so_far = []
+
+        for l in lines:
+            # New choice?
+            if l.lstrip()[:2] == '* ':
+                # push old choice (iff non-empty)
+                if len(choice_lines_so_far) > 0:
+                    new_lines.append((choice_name, choice_lines_so_far))
+                # switch to new choice
+                choice_name = l.lstrip()[2:]
+                choice_lines_so_far = []
+            else:
+                choice_lines_so_far.append(l.lstrip())
+
+        # add the last one (iff non-empty)
+        if len(choice_lines_so_far) > 0:
+            new_lines.append((choice_name, choice_lines_so_far))
+
+        # add heading (iff non-empty)
+        if len(new_lines) > 0:
+            d2[heading] = new_lines
+
+    return d2
 
 
 class SimpleDialogue(object):
@@ -32,3 +101,4 @@ class SimpleDialogue(object):
 
     def update(self):
         pass
+
