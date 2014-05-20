eda0d48d6ad1f2d3403241a5ac5a91da3b49f1aa
Oliver Middleton <olliemail27@gmail.com>
2014-05-20 22:06:58 +0100

    add pause support to movies
    either space key or left mouse button

--- gslib/engine/movie.py
+++ gslib/engine/movie.py
@@ -12,6 +12,17 @@ class MoviePlayer(pyglet.event.EventDispatcher):
     def __init__(self):
         self._player = None
 
+        # TODO: THIS IS A HACK TO WORK AROUND on_eos BEING BROKEN
+        self._playing = False
+
+    def _toggle_pause(self):
+        if self._playing:
+            self._player.pause()
+            self._playing = False
+        else:
+            self._player.play()
+            self._playing = True
+
     def start(self, movie):
         try:
             video_source = pyglet.media.load(os.path.join(VIDEO_DIR, movie))
@@ -23,6 +34,7 @@ class MoviePlayer(pyglet.event.EventDispatcher):
         self._player.queue(video_source)
         self._player.push_handlers(on_eos=self.stop)
         self._player.play()
+        self._playing = True
         window.push_handlers(self)
 
     def stop(self):
@@ -31,12 +43,13 @@ class MoviePlayer(pyglet.event.EventDispatcher):
             self._player.pause()
             self._player.remove_handlers(on_eos=self.stop)
             self._player = None
+        self._playing = False
         self.dispatch_event('on_movie_end')
 
     def draw(self):
 
         # TODO: THIS IS A HACK TO WORK AROUND on_eos BEING BROKEN
-        if not self._player.playing:
+        if not self._player.playing and self._playing:
             self.stop()
             return
 
@@ -46,6 +59,14 @@ class MoviePlayer(pyglet.event.EventDispatcher):
     def on_key_press(self, symbol, modifiers):
         if symbol == pyglet.window.key.ESCAPE:
             self.stop()
+            return pyglet.event.EVENT_HANDLED
+        elif symbol == pyglet.window.key.SPACE:
+            self._toggle_pause()
+            return pyglet.event.EVENT_HANDLED
+
+    def on_mouse_press(self, x, y, button, modifiers):
+        if button == pyglet.window.mouse.LEFT:
+            self._toggle_pause()
             return pyglet.event.EVENT_HANDLED
 
 MoviePlayer.register_event_type('on_movie_end')
